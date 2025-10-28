b01a23e669ae943117f3f13ce36eeecb2dd76192
Raven <rjsnavarette@gmail.com>
2025-10-29 05:00:57 +0800

    Stop tracking hashes

--- verify_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hash = '$2y$10$V5FFaUtkgkDwfZfIDIS/JehXW4mfOiR29dMhNYowFarxyFhCO5je2';
-if (password_verify('admin1234', $hash)) {
-    echo "✅ Password matches!";
-} else {
-    echo "❌ Password invalid.";
-}
-?>
