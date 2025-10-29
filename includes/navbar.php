ceeda7ec508a41b2d230006242ce9f0cfafb7242
Raven <rjsnavarette@gmail.com>
2025-10-30 02:48:02 +0800

    add navbar logo

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,3 +1,3 @@
 <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="/index.php">Virtual Ventures</a>
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="/index.php"><img src="/assets/images/vvs-transparent-logo.png" alt="VVS Logo" height="50"/></a>
 </header>
